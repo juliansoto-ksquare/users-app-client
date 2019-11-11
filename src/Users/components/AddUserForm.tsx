8ef73b0691954bb32f422c373eb5f5d4777acd28
juliansoto <juliansotoponce@gmail.com>
2019-11-11 12:25:52 -0600

    add styles to form to add users

--- src/Users/components/AddUserForm.tsx
+++ src/Users/components/AddUserForm.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export interface IUserFormProps {
     onSubmit: (event: React.FormEvent) => void;
 }
 
+const Label = styled.label`
+    display: flex;
+    flex-direction: column;
+    font-size: 1em;
+    margin-bottom: 1em;
+`;
+
+const Input = styled.input`
+    background: white;
+    margin-top: 0.5em;
+    padding: 1em;
+    border: solid 1px black;
+    border-radius: 0.5em;
+`;
+
+const Button = styled.button`
+    padding: 1em;
+    width: 100%;
+    font-size: 1em;
+    border: solid 2px black;
+    border-radius: 0.5em;
+    background: #3ba5ff;
+    font-weight: bold;
+`;
+
 export const AddUserForm: React.FC<IUserFormProps> = ({onSubmit}) => {
     return (
         <div>
+            <h1>Add user</h1>
             <form onSubmit={onSubmit}>
-                <label>
+                <Label>
                     First name:
-                    <input name="firstName" type="text" required />
-                </label>
-                <label>
+                    <Input name="firstName" type="text" required />
+                </Label>
+                <Label>
                     Last name:
-                    <input name="lastName" type="text" required />
-                </label>
-                <label>
+                    <Input name="lastName" type="text" required />
+                </Label>
+                <Label>
                     Age:
-                    <input name="age" type="number" required />
-                </label>
-                <button>Create</button>
+                    <Input name="age" type="number" required />
+                </Label>
+                <Button>Create</Button>
             </form>
         </div>
     )
